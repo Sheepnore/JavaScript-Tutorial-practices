481404972e24fd64cd9730bf1fb41e540a8840aa
Sheepnore <a83840066@gmail.com>
2024-06-09 01:06:58 +0800

    Redo exercise 10g, it was a bit challenging the first time doing, now it's became easier.

--- Javascript/10-exercise/10-JS/10d.js
+++ Javascript/10-exercise/10-JS/10d.js
@@ -1,19 +1,18 @@
 function toggled(classSelector){
 
   const button = document.querySelector(classSelector);
-  if (!button.classList.contains('is-toggled')){
 
-    turnOffToggle();
-
-    button.classList.add('is-toggled');
+  if (button.classList.contains('is-toggled') == false){
+    unToggled();
     
+    button.classList.add('is-toggled');
   }
   else{
     button.classList.remove('is-toggled');
   }
 }
 
-function turnOffToggle(){
+function unToggled(){
   const turnOff = document.querySelector('.is-toggled');
   if (turnOff){
     turnOff.classList.remove('is-toggled');
